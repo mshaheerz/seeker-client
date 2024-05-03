8e43cc320290804f8fad46fe8da993b07a6022c7
mohammed shaheer kp <72137242+mshaheerz@users.noreply.github.com>
2024-05-03 16:25:03 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 
 
-export const baseUrl ="https://api.buyelectro.shop/api/";
+export const baseUrl ="https://seeker-api.onrender.com/api/";
 // export const baseUrl = "http://localhost:3001/api/";
 // export const usertoken = {"usertoken":JSON.parse(localStorage.getItem("usertoken"))}
 // export const admintoken = {'admintoken':localStorage.getItem('admintoken')}
